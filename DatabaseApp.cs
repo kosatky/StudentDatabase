455c7a5ef3a20f829d5057eef7477c3a1866a3e3
matej <matej.kostka@kostkovi.cz>
2025-04-24 11:14:23 +0200

    Features

--- DatabaseApp.cs
+++ DatabaseApp.cs
@@ -1,11 +1,38 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
+
 namespace StudentDatabase
 {
 	internal class DatabaseApp
 	{
 		public void Run()
 		{
-		}
-	}
+            ShowCourse(1);
+        }
+
+        public void ShowCourse(int id)
+        {
+            using var dbContext = new AppDbContext();
+            var course = dbContext.Courses.FirstOrDefault(x => x.Id == id);
+            if (course != null)
+            {
+                Console.WriteLine($"{course.CourseName} {course.Department} {course.Description}");
+            }
+        }
+
+        public void CreateCourse()
+        {
+            var course = new Course
+            {
+                CourseName = "English",
+                Department = "Languages",
+                Description = "English language"
+
+            };
+            using var dbContext = new AppDbContext();
+            dbContext.Add(course);
+            dbContext.SaveChanges();
+        }
+    }
 }
 
